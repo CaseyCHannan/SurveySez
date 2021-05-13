19f9f814ae710522955f1155550e4166b6e8526c
CHannan <hannan.casey@gmail.com>
2021-05-13 18:58:46 -0400

    add front end. add proxy via concurrently

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         BLRODOOD <code>src/App.js</code> Filler.
+        </p>
+        <a
+          href="/auth/google"
+        >
+         This ain't working
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
