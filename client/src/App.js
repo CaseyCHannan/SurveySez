c56d1eeadc99ed213843aa707240b0f84475d096
CHannan <hannan.casey@gmail.com>
2021-05-17 18:51:31 -0400

    add action and reducer folders and files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,6 @@
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={null} className="App-logo" alt="logo" />
-//         <p>
-//          BLRODOOD <code>src/App.js</code> Filler.
-//         </p>
-//         <a
-//           href="/auth/google"
-//         >
-//          This ain't working
-//         </a>
-//       </header>
-//     </div>
-//   );
+
 // }
 
 // export default App;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
-const Header = () => <h2>HEader</h2>
-const Dogtime = () => <h2>Dog time</h2>
+import Header from './Header'
+
+
 const Surveys = () => <h2>NEW!!</h2>
 const Potent = () => <h2>potent potables</h2>
 
-const App = () => {
-  return (
-    <div>
-        <BrowserRouter> 
-        <>
-            <Route path="/" component={Potent} />
-            <Route path="/surveys" component={Surveys} />
-        </>    
-        </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount(){
+    this.props.fetchUser()
+  }
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter> 
+                <>
+                    <Header/>
+                    <Route path="/" component={Potent} />
+                    <Route path="/surveys" component={Surveys} />
+                </>    
+                </BrowserRouter>
+            </div>
+        );
+    };
+}
 
-export default App;
+export default connect(null, actions) (App);
