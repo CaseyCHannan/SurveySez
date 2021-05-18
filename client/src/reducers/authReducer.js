9a2c81a5e08a2ca7bdd403b9e2fb308ead5d88d6
CHannan <hannan.casey@gmail.com>
2021-05-17 22:26:53 -0400

    begin logout on header

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,12 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default function(state ={}, action) {
-  console.log(action)
-    switch(action.type) {
-        default:
-          return state  
-    }
+/* eslint-disable import/no-anonymous-default-export */
 
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
+  switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+    default:
+      return state;
+  }
 }
